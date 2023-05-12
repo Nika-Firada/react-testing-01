1a4e8d4e9847e050edb2991fb367ca75c381507f
Nika-Firada <nika.firada@gmail.com>
2023-05-12 13:18:18 +0400

    second phase: testing filter

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -4,14 +4,19 @@ import List from '../list'
 import Search from '../search/Search';
 
 
-const data = ['html', 'css', 'javascript', 'typescript', 'react', 'vue', 'angular'];
+const data = ['html', 'css', 'javascript', 'typescript', 'React', 'vue', 'angular'];
 function App() {
-  const [search,setSearch] = useState('')
+  const [search,setSearch] = useState('');
+  const [items,setItems] = useState(data);
+
+  useEffect(() => {
+    setItems(data.filter(el => el.toLowerCase().includes(search.toLowerCase())));
+  },[search])
   return (
     <div className="App">
       <div className="App-header">
         <Search children="Find Course" value={search} onChange={(e) => setSearch(e.target.value)} />
-        <List items={data} />
+        <List items={items} />
       </div>
     </div>
   );
--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -1,12 +1,35 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
 import App from './App';
-
+import userEvent from '@testing-library/user-event'; 
 
 // 'test name', () => {test description}
 describe('renders', () => {
     it('app render', () => {
         render(<App />);
-        // screen.debug();
-        expect(screen.getByRole('list')).toBeInTheDocument;
-    })
+        expect(screen.getByRole('list')).toBeInTheDocument();
+        expect(screen.getByText('Find Course')).toBeInTheDocument();
+    });
+    it('typing in searchbox works', () => {
+        render(<App />);
+        expect(screen.queryByDisplayValue(/React/)).toBeNull();
+        userEvent.type(screen.getByRole('textbox'), 'React');
+        expect(screen.queryByDisplayValue(/React/)).toBeInTheDocument();
+    });
+    it('search filter is working', () => {
+        render(<App />);
+        expect(screen.getByText('vue')).toBeInTheDocument();
+        expect(screen.getByText('javascript')).toBeInTheDocument();
+        // userEvent.type(screen.getByRole('textbox'), 'script');
+        // async
+        act(() => {
+            userEvent.type(screen.getByRole('textbox'), 'script');
+        });
+        expect(screen.queryByText(/vue/)).toBeNull();
+        expect(screen.queryByText('javascript')).toBeInTheDocument();
+    });
+    test('App snap', () => {
+        const appComponent = render(<App />)
+        // creates snapshot
+        expect(appComponent).toMatchSnapshot();
+    });
 });
